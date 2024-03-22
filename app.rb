41ddea4867c9dab4def8ed3add48ec3b6a2910ff
Yana Ninovska <125706832+yananin@users.noreply.github.com>
2024-03-22 02:13:43 +0000

    created game app

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get('/') do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get('/rock') do
+  @option=["rock", "paper", "scissors"].sample
+  erb(:rock )
+end
+
+get('/paper') do
+  @option=["rock", "paper", "scissors"].sample
+  erb(:paper)
+end
+
+get('/scissors') do
+  @option=["rock", "paper", "scissors"].sample
+  erb(:scissors)
 end
